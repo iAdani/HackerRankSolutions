d0d6a0b0b997962cf0b89efea583a7c0c9d6f6c2
iAdani <guy3102@gmail.com>
2024-08-05 20:31:40 +0300

    Big Sorting

---
+++ Big_Sorting.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'bigSorting' function below.
+     *
+     * The function is expected to return a STRING_ARRAY.
+     * The function accepts STRING_ARRAY unsorted as parameter.
+     */
+
+    public static List<String> bigSorting(List<String> unsorted) {
+        List<String> solution = new ArrayList();
+        Map<Integer, List<String>> lengthMap = new HashMap();
+        for (String number : unsorted) {
+            int len = number.length();
+            if (!lengthMap.containsKey(len))
+                lengthMap.put(len, new ArrayList());
+            lengthMap.get(len).add(number);
+        }
+        
+        List<Integer> lengths = new ArrayList(lengthMap.keySet());
+        Collections.sort(lengths);
+        
+        for (Integer key : lengths) {
+            Collections.sort(lengthMap.get(key));
+            solution.addAll(lengthMap.get(key));
+        }
+        
+        return solution;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<String> unsorted = IntStream.range(0, n).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .collect(toList());
+
+        List<String> result = Result.bigSorting(unsorted);
+
+        bufferedWriter.write(
+            result.stream()
+                .collect(joining("\n"))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
